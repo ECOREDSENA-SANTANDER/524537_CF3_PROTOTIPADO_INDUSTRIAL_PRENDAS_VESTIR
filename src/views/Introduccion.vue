43370e482b8ef8ca58ee4475e90b500c00273b6e
Andrea Botello <abotellod@gmail.com>
2026-03-11 16:04:29 -0500

    Plantilla

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -2,12 +2,34 @@
 .curso-main-container.introduccion
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido(data-aos="flip-up")
+  
+    .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span
           i.fas.fa-info
       h1 Introducción
 
+    p En el siguiente video se presenta una breve explicación de los temas que serán tratados en este componente: 
+    figure.mb-5(data-aos="zoom-in-left")
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/gSoxcc4J2To" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Patronaje básico 
+    .titulo-principal.color-acento-contenido
+      h1 Patronaje básico 
+    .row.justify-content-center.mb-5(data-aos="zoom-in-left")
+      .col-lg-8
+        p Para comenzar con el trazado de patrones desde los básicos en prendas inferiores, es crucial establecer la línea de aplomo o línea base de construcción, que se corresponde con la altura de cadera o tiro. Del mismo modo, al trazar patrones desde los básicos en prendas superiores, la línea de aplomo o línea base de construcción se sitúa en la línea de sisa o la altura de cadera.
+        p Es importante tener en cuenta que las medidas presentadas en los videos son ejemplos y orientaciones únicamente. Cada diseñador puede tener referencias distintas, dependiendo del tipo de trazado, elementos utilizados e incluso del tipo de herramienta de dibujo. Sin embargo, siempre es fundamental hacer referencia a las medidas del cuadro de tallas y a las fórmulas pertinentes.
+        p Es esencial también reconocer que en el ámbito comercial se utilizan diversos nombres para describir las siluetas y formas de las prendas. No obstante, en este proceso de aprendizaje nos enfocaremos en aspectos funcionales sin entrar en especificaciones de nombres comerciales.
+      .col-sm-5.col-lg-3.col-8
+        img(src='@/assets/curso/temas/tema1/img01.svg', alt='')
+    
+    .cajon.color-acento-contenido.p-4.mb-5
+      .row.justify-content-center.align-items-center(data-aos="zoom-in-left")
+        .col-lg-1.p-2.col-6.col-sm-3
+          img(src='@/assets/curso/temas/tema1/img02.svg', alt='')
+        .col-lg-11
+          p Al tener en cuenta los términos de los estudiosos se puede determinar que: satisfacción, cliente, necesidades, mercado y expectativas, son las palabras comunes que engloban el concepto para concluir que la calidad es el nivel de satisfacción del cliente, al cubrir sus necesidades y expectativas. 
 </template>
 
 <script>
@@ -16,14 +38,6 @@ export default {
   data: () => ({
     // variables de vue
   }),
-  mounted() {
-    this.$nextTick(() => {
-      this.$aosRefresh()
-    })
-  },
-  updated() {
-    this.$aosRefresh()
-  },
 }
 </script>
 
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,15 +2,16 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum.
+
     .row.justify-content-center
-      .col-lg-10.mb-5.bgs.p-4.brad
+      .col-lg-12.mb-5
+        p La base fundamental para el patronaje de prendas es la apropiación de los pasos requeridos para realizarlo, el manejo de las proporciones y el análisis de cada pieza que acompaña la prenda. En este componente se realiza un recorrido por las características de básicos en línea infantil, masculina y femenina, detallando en cada una, herramientas teóricas y prácticas para la producción de prendas.
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="La imagen muestra un diagrama con múltiples nodos conectados, centrado en el tema del “Patronaje Básico”. Este nodo principal se conecta a varias categorías relacionadas con la confección y el diseño de ropa. A su alrededor, se ramifican nodos específicos para diferentes líneas y tipos de prendas: Línea Masculina: Incluye nodos para “Base Sastre”, “Jean Slim Fit”, “Pantalón Clásico” y “Camisa Clásica”. Línea Femenina: Conecta con “Chaquetas y Vestidos” y “Faldas y Overoles”. Línea Infantil: Se desglosa en “Patrón a Escala”, “Prendas Superiores”, “Overol” y “Prendas Inferiores”. También aparece un nodo para “Pinza Correctiva”, lo que sugiere ajustes y modificaciones en el diseño para un mejor ajuste. El diagrama sirve para visualizar cómo se estructura el patronaje en diferentes categorías y cómo se interrelacionan entre sí dentro del ámbito del diseño de moda, posiblemente utilizado como herramienta educativa o de planificación en el diseño de vestuario.")
       .col-auto
-        a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
+        a.anexo.mb-5(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
-            img(src="@/assets/bullets/icono-pdf.svg")
+            img(src="@/assets/template/icono-pdf.svg")
           .anexo__texto
             p Anexo. Síntesis
 
